81cf64b3609574b7587448a3bfde540556032d8b
Pranaav Sriram <srirampranaav@gmail.com>
2020-05-25 00:00:35 -0500

    Updated the Multiplier for the Fabric View Controller

--- Project Estimation/FabricViewController.swift
+++ Project Estimation/FabricViewController.swift
@@ -15,19 +15,21 @@ class FabricViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    var len = 0
+    var length = 0
     var area = 0
     var width = 0
     var price = 0
+    
+    //The price is determined by multiplying the desired area by an industry average multiplier. This will ensure that our estimation will be within normal range.
     func getArea(){
         
-        area = len * width
+        area = length * width
         AreaLabel.text = String(area)
         
     }
     
     func getPrice(){
-        price = area * 50 //Dummy Multiplier
+        price = area * Int(0.50)
         PriceLabel.text = String(price)
     }
     
@@ -35,30 +37,33 @@ class FabricViewController: UIViewController {
     @IBAction func BackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    @IBOutlet weak var LengthLabel: UILabel!
     
+    @IBOutlet weak var LengthLabel: UILabel!
     @IBOutlet weak var WidthLabel: UILabel!
     @IBOutlet weak var PriceLabel: UILabel!
     @IBOutlet weak var AreaLabel: UILabel!
     @IBOutlet weak var LengthTextField: UITextField!
-    
     @IBOutlet weak var WidthTextField: UITextField!
+    
+    
     @IBAction func WidthEntered(_ sender: Any) {
         WidthLabel.text = WidthTextField.text
          width = (WidthLabel.text! as NSString).integerValue
-        if(len != 0 && width != 0){
+        if(length != 0 && width != 0){
                    getArea()
                    getPrice()
                }
          
     }
+    
+    
     @IBAction func LengthEntered(_ sender: Any) {
         LengthLabel.text = LengthTextField.text
-               len = (LengthLabel.text! as NSString).integerValue
-                      if(len != 0 && width != 0){
+               length = (LengthLabel.text! as NSString).integerValue
+                      if(length != 0 && width != 0){
                           getArea()
                           getPrice()
-    }
+        }
     }
     /*
     // MARK: - Navigation
